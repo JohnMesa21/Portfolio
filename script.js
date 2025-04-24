5cdf9aec72df4e635b4dadb5a50a23d4e33283af
Ander <128411538+JohnMesa21@users.noreply.github.com>
2025-04-23 22:26:22 -0500

    Actualizacion en apartado certificados, animaciones se scroll

--- script.js
+++ script.js
@@ -38,4 +38,26 @@ function efectoHabilidades(){
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
-} 
+}
+
+// Animación al hacer scroll
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Para asegurar que la animación se ejecute al cargar la página
+reveal(); 
